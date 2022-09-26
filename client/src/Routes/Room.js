9f742dc7d436c873a18ba3aa4f11bbf6c81a928e
Rehan Ali Shah <rehan.shah70@yahoo.com>
2022-09-26 14:04:22 +0500

    Functionalities added.
    Now user can show or hide video and audio. And leave the call.

--- client/src/Routes/Room.js
+++ client/src/Routes/Room.js
@@ -13,24 +13,22 @@ const useMicrophoneAndCameraTracks = createMicrophoneAndCameraTracks()
 const appId = process.env.REACT_APP_API_ID
 const token = process.env.REACT_APP_API_TOKEN || null
 
-const remoteUsers = {}
-let localTracks = []
-
 const Room = ({ name, room, uid }) => {
 	const client = useClient()
 	const { ready, tracks } = useMicrophoneAndCameraTracks()
-	// const [elementsId, setElementsId] = useState([])
-	// const [tracks, setTracks] = useState([])
+
+	const [video, setVideo] = useState(true)
+	const [audio, setAudio] = useState(true)
+	const [screen, setScreen] = useState(false)
+
 	const [start, setStart] = useState(true)
 	const [users, setUsers] = useState([])
 
 	useEffect(() => {
-		// function to initialise the SDK
 		let init = async name => {
-			console.log('init', name)
 			client.on('user-published', async (user, mediaType) => {
 				await client.subscribe(user, mediaType)
-				console.log('subscribe success')
+
 				if (mediaType === 'video') {
 					setUsers(prevUsers => {
 						return [...prevUsers, user]
@@ -42,7 +40,6 @@ const Room = ({ name, room, uid }) => {
 			})
 
 			client.on('user-unpublished', (user, type) => {
-				console.log('unpublished', user, type)
 				if (type === 'audio') {
 					user.audioTrack?.stop()
 				}
@@ -54,7 +51,6 @@ const Room = ({ name, room, uid }) => {
 			})
 
 			client.on('user-left', user => {
-				console.log('leaving', user)
 				setUsers(prevUsers => {
 					return prevUsers.filter(User => User.uid !== user.uid)
 				})
@@ -66,15 +62,42 @@ const Room = ({ name, room, uid }) => {
 		}
 
 		if (ready && tracks) {
-			console.log('init ready')
 			init(room)
 		}
 	}, [room, client, ready, tracks])
+
+	const Videofunc = async () => {
+		await tracks[1].setEnabled(!video)
+		setVideo(!video)
+	}
+
+	const Audiofunc = async () => {
+		await tracks[0].setEnabled(!audio)
+		setVideo(!audio)
+	}
+
+	const Leavefunc = () => {
+		window.location.reload()
+	}
 
 	return (
 		<div className='room'>
 			<div className='videosection' id='videosection'>
 				{start && tracks && <Videos users={users} tracks={tracks} />}
+				<div className='controls'>
+					<div className='icon' style={{ backgroundColor: video ? 'blueviolet' : '' }} onClick={Videofunc}>
+						<i className='gg-webcam' style={{ scale: '1.5' }}></i>
+					</div>
+					<div className='icon' style={{ backgroundColor: audio ? 'blueviolet' : '' }} onClick={Audiofunc}>
+						<i className='gg-mic' style={{ transform: 'scaleY(0.9)', marginTop: '2px' }}></i>
+					</div>
+					<div className='icon' style={{ backgroundColor: screen ? 'blueviolet' : '' }}>
+						<i className='gg-screen' style={{ transform: 'scale(1.3)', marginTop: '-3px' }}></i>
+					</div>
+					<div className='icon' style={{ backgroundColor: 'red', border: 'none' }} onClick={Leavefunc}>
+						<i className='gg-export' style={{ transform: 'scale(1.3) rotate(-90deg)' }}></i>
+					</div>
+				</div>
 			</div>
 			<div className='msgsection'></div>
 		</div>
