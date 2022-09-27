37ff43f0d3c514ace99794057ccfd979a9002fac
Rehan Ali Shah <rehan.shah70@yahoo.com>
2022-09-27 20:11:29 +0500

    full screen video on double click added

--- client/src/Components/Videos.js
+++ client/src/Components/Videos.js
@@ -4,9 +4,24 @@ import { AgoraVideoPlayer } from 'agora-rtc-react'
 const Videos = ({ users, tracks }) => {
 	const videoTrack = tracks.length ? tracks[1] : tracks
 
+	const fullScreen = e => {
+		const ele = document.getElementById(e.target.id)
+
+		if (ele) {
+			ele.requestFullscreen()
+		}
+	}
+
+	let id = 1
 	return (
 		<React.Fragment>
-			<AgoraVideoPlayer className='vid' videoTrack={videoTrack} style={{ height: '95%', width: '95%' }} />
+			<AgoraVideoPlayer
+				className='vid'
+				id={`player${id++}`}
+				videoTrack={videoTrack}
+				style={{ height: '95%', width: '95%' }}
+				onDoubleClick={fullScreen}
+			/>
 			{users.length > 0 &&
 				users.map(user => {
 					if (user.videoTrack) {
@@ -16,6 +31,8 @@ const Videos = ({ users, tracks }) => {
 								videoTrack={user.videoTrack}
 								style={{ height: '95%', width: '95%' }}
 								key={user.uid}
+								id={`player${id++}`}
+								onDoubleClick={fullScreen}
 							/>
 						)
 					} else return null
